aa242c2b78841386bdf5fbf0002f0ad535b75636
JoaoGretter <joao.gretter@sicoob.com.br>
2026-03-18 20:35:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Deploy automático teste funcionando 🚀"
+    return "Deploy automático teste 2 funcionando 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
